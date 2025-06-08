92f31b5ae5074d375eb1ff7fc9f3c8306c24820c
hlab <subhranshupati0412@gmail.com>
2025-06-08 17:33:11 +0000

    fix: enabled cors for network call

--- homelab_mcp/main.py
+++ homelab_mcp/main.py
@@ -1,12 +1,27 @@
 from fastmcp import FastMCP
 from servers import resume_mcp
+from starlette.requests import Request
+from starlette.responses import PlainTextResponse
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 import asyncio
+import uvicorn
 
 main_mcp = FastMCP(name="homelab")
+
+@main_mcp.custom_route("/health", methods=["GET"])
+async def health_check(request: Request) -> PlainTextResponse:
+    return PlainTextResponse("OK")
+
+custom_middleware = [
+    Middleware(CORSMiddleware, allow_origins=["*"]),
+]
+
+http_app = main_mcp.http_app(middleware=custom_middleware)
 
 async def setup():
     await main_mcp.import_server("resume", resume_mcp)
 
 if __name__ == "__main__":
     asyncio.run(setup())
-    main_mcp.run()
+    uvicorn.run(http_app, host="0.0.0.0", port=4200)
