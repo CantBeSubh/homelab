822a19660cebe84747b6019603f5dce0a4e11965
Subhranshu Pati <subhranshupati0412@gmail.com>
2025-06-09 00:29:27 +0530

    chore: linting

--- homelab_mcp/main.py
+++ homelab_mcp/main.py
@@ -1,17 +1,20 @@
+import asyncio
+
+import uvicorn
 from fastmcp import FastMCP
 from servers import resume_mcp
-from starlette.requests import Request
-from starlette.responses import PlainTextResponse
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
-import asyncio
-import uvicorn
+from starlette.requests import Request
+from starlette.responses import PlainTextResponse
 
 main_mcp = FastMCP(name="homelab")
+
 
 @main_mcp.custom_route("/health", methods=["GET"])
 async def health_check(request: Request) -> PlainTextResponse:
     return PlainTextResponse("OK")
+
 
 custom_middleware = [
     Middleware(CORSMiddleware, allow_origins=["*"]),
@@ -19,8 +22,10 @@ custom_middleware = [
 
 http_app = main_mcp.http_app(middleware=custom_middleware)
 
+
 async def setup():
     await main_mcp.import_server("resume", resume_mcp)
+
 
 if __name__ == "__main__":
     asyncio.run(setup())
