632ef7fe9c4e71859bcd8c226fecd5c707a11ddf
hlab <subhranshupati0412@gmail.com>
2025-06-08 17:56:09 +0000

    fix: update resume path

--- homelab_mcp/servers/resume/main.py
+++ homelab_mcp/servers/resume/main.py
@@ -7,7 +7,7 @@ resume_mcp = FastMCP("resume")
 @resume_mcp.tool
 def get_resume() -> str:
     """Get Subhranshu's Resume"""
-    reader = pypdf.PdfReader(f'{os.getcwd()}\\resources\\Subhranshu_Resume_2025.pdf')
+    reader = pypdf.PdfReader(f'{os.getcwd()}/resources/Subhranshu_Resume_2025.pdf')
     text = ""
     for page in reader.pages:
         text += page.extract_text()
