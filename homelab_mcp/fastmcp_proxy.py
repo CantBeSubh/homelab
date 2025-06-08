79df9f5034a1ca0abedded859826aa8e7a3cf03d
Subhranshu Pati <subhranshupati0412@gmail.com>
2025-06-09 00:33:06 +0530

    feat: use nginx proxy

--- homelab_mcp/fastmcp_proxy.py
+++ homelab_mcp/fastmcp_proxy.py
@@ -1,10 +1,6 @@
 from fastmcp import FastMCP
 
-# Create a proxy to a remote server
-proxy = FastMCP.as_proxy(
-    "http://192.168.1.25:4200/mcp", 
-    name="Homelab Proxy"
-)
+proxy = FastMCP.as_proxy("https://mcp.homelab.subhranshu.com/mcp", name="Homelab Proxy")
 
 if __name__ == "__main__":
-    proxy.run() 
+    proxy.run()
